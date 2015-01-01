bee88ae207636bfaa549823121dd88e2d7bb1064
FeliciousX <feliciousx@gmail.com>
2015-01-02 01:54:46 +0800

    Adds bourbon mixin library Updates .gitignore Uses sass

--- application/views/templates/head.php
+++ application/views/templates/head.php
@@ -2,4 +2,4 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?=base_url('public/css/head.css')?>" type="text/css">
 <link rel="stylesheet" href="<?=base_url('public/assets/bootstrap/css/bootstrap.min.css')?>" type="text/css">
-<link rel="stylesheet" href="<?=base_url('public/assets/bootstrap/css/bootstrap-theme.css')?>" type="text/css">
+<link rel="stylesheet" href="<?=base_url('public/assets/bootstrap/css/bootstrap-theme.min.css')?>" type="text/css">
