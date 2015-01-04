767d739de0c9f68bb585885b18b5482ab120a06c
FeliciousX <feliciousx@gmail.com>
2015-01-05 01:36:36 +0800

    Adds new compiled bootstrap library for navbar Navbar uses bootstrap css now Adds new links in the config/links.php

--- application/config/links.php
+++ application/config/links.php
@@ -1,4 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+// link of different pages
+$config['href']['search'] = 'search';
+$config['href']['map'] = 'map';
 
 // This config contains all the necessary links to specific functions
 $config['href']['places']['details'] = 'places/details';
--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,13 +1,15 @@
-<nav class="bg-primary">
-    <div class="btn-group btn-group-justified" role="group" aria-label="navigation">
-        <a class="btn" href="">
-            <img class="nav-btn" src="<?=base_url('public/images/icons/search.png')?>">
-        </a>
-        <a class="btn" href="<?=site_url()?>">
-            <img class="nav-btn" src="<?=base_url('public/images/icons/home.png')?>">
-        </a>
-        <a class="btn" href="">
-            <img class="nav-btn" src="<?=base_url('public/images/icons/box_button.png')?>">
-        </a>
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
+    <div class="container">
+        <div class="btn-group btn-group-justified" role="group" aria-label="navigation">
+            <a class="btn" href="<?=site_url($href['search'])?>">
+                <img class="nav-btn" src="<?=base_url('public/images/icons/search.png')?>">
+            </a>
+            <a class="btn" href="<?=site_url()?>">
+                <img class="nav-btn" src="<?=base_url('public/images/icons/home.png')?>">
+            </a>
+            <a class="btn" href="<?=site_url($href['map'])?>">
+                <img class="nav-btn" src="<?=base_url('public/images/icons/box_button.png')?>">
+            </a>
+        </div>
     </div>
 </nav>
