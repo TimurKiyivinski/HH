c52f77e82d1a469f64943686b9f4c3def7dc771b
Sean <4308131@students.swinburne.edu.my>
2015-01-14 02:22:38 +0800

    search page done (Static)

--- application/views/search.php
+++ application/views/search.php
@@ -10,7 +10,7 @@
 
 <div class="container">
 
-    <div class="row search-banner bg-primary">
+    <div class="row search-bar bg-primary">
         <div class="col-xs-12 col-sm-12 col-dm-12">
             <?=form_open(current_url(), array('method' => 'get', 'class' => 'form-horizontal', 'role' => 'form'))?>
                 <div class="input-group">
@@ -75,6 +75,135 @@
                 <?php endforeach ?>
             </div><!-- /.list-group -->
         </div><!-- /.panel-primary -->    
+
+        <!-- //////////////////////////////////////////////////////////////////////////////////////////// -->
+        <div class="panel panel-primary" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel-heading" role="tab" id="headingSOMETHINGCOOL">
+                <a class = "white"data-toggle="collapse" data-parent="#accordion" href="#collapseSOMETHINGCOOL" aria-expanded="true" aria-controls="collapseSOMETHINGCOOL">
+                    <h3 class="panel-title">SOMETHINGCOOL</h3>
+                </a>
+            </div><!-- /.panel-heading -->
+
+            <div class="list-group panel-collapse collapse in" id="collapseSOMETHINGCOOL" role="tabpanel" aria-labelledby="headingSOMETHINGCOOL">
+                <?php foreach ($places as $place): ?>
+                <a class="list-group-item panel-body" href="<?=site_url($href['places']['details'].'/'.$category['id'].'/'.$place['id'])?>">
+                    <div class="row search-panel">
+                        <div class="col-xs-3 col-sm-2 col-md-2">
+                            <img class="img-thumbnail place-panel-img" src="<?=base_url('public/images/places/thumbnails/Merdeka_Palace_Hotel_thumb.png')?>" alt="<?=$place['name']?>">
+                        </div><!-- col-xs-3 -->
+                        <div class="col-xs-9 col-sm-8 col-md-8 place-panel-detail">
+                            <div class="row">
+                                <div class="col-xs-12 col-md-12 place-panel-name">
+                                    <h4><?=$place['name']?></h4>
+                                </div><!-- place-panel-name -->
+                            </div><!-- /.row -->
+                            <div class="row">
+                                <div class="col-xs-12 col-md-12 place-panel-rating">
+                                    <h4>
+                                    <?php for ($i = 0; $i < 5; $i++): ?>
+                                        <?php if ($place['rating'] > 0): ?>
+                                            <span class="glyphicon glyphicon-star"></span>
+                                        <?php else: ?>
+                                            <span class="glyphicon glyphicon-star-empty"></span>
+                                        <?php endif; ?>
+                                        <?php $place['rating']--; ?>
+                                    <?php endfor; ?>
+                                    </h4>
+                                </div><!-- /.col-xs-12 -->
+                            </div><!-- /.row -->
+                        </div><!-- /. place-panel-detail -->
+                        <div class="hidden-xs col-sm-2 col-md-2">
+                            <h1><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></h1>
+                        </div><!-- /.hidden-xs -->
+                    </div><!-- /.search-panel -->
+
+                </a>
+
+
+                <?php endforeach ?>
+            </div><!-- /.list-group -->
+        </div><!-- /.panel-primary -->   
+        <div class="panel panel-primary" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel-heading" role="tab" id="headingSOMETHINGCOOL2">
+                <a class = "white"data-toggle="collapse" data-parent="#accordion" href="#collapseSOMETHINGCOOL2" aria-expanded="true" aria-controls="collapseSOMETHINGCOOL2">
+                    <h3 class="panel-title">SOMETHINGCOOL2</h3>
+                </a>
+            </div><!-- /.panel-heading -->
+
+            <div class="list-group panel-collapse collapse in" id="collapseSOMETHINGCOOL2" role="tabpanel" aria-labelledby="headingSOMETHINGCOOL2">
+                <?php foreach ($places as $place): ?>
+                <a class="list-group-item panel-body" href="<?=site_url($href['places']['details'].'/'.$category['id'].'/'.$place['id'])?>">
+                    <div class="row search-panel">
+                        <div class="col-xs-3 col-sm-2 col-md-2">
+                            <img class="img-thumbnail place-panel-img" src="<?=base_url('public/images/places/thumbnails/Merdeka_Palace_Hotel_thumb.png')?>" alt="<?=$place['name']?>">
+                        </div><!-- col-xs-3 -->
+                        <div class="col-xs-9 col-sm-8 col-md-8 place-panel-detail">
+                            <div class="row">
+                                <div class="col-xs-12 col-md-12 place-panel-name">
+                                    <h4><?=$place['name']?></h4>
+                                </div><!-- place-panel-name -->
+                            </div><!-- /.row -->
+                            <div class="row">
+                                <div class="col-xs-12 col-md-12 place-panel-rating">
+                                    <h4>
+                                    <?php for ($i = 0; $i < 5; $i++): ?>
+                                        <?php if ($place['rating'] > 0): ?>
+                                            <span class="glyphicon glyphicon-star"></span>
+                                        <?php else: ?>
+                                            <span class="glyphicon glyphicon-star-empty"></span>
+                                        <?php endif; ?>
+                                        <?php $place['rating']--; ?>
+                                    <?php endfor; ?>
+                                    </h4>
+                                </div><!-- /.col-xs-12 -->
+                            </div><!-- /.row -->
+                        </div><!-- /. place-panel-detail -->
+                        <div class="hidden-xs col-sm-2 col-md-2">
+                            <h1><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></h1>
+                        </div><!-- /.hidden-xs -->
+                    </div><!-- /.search-panel -->
+
+                </a>
+                <?php endforeach ?>
+
+                <!-- remove loop as well during refactor -->
+                <?php foreach ($places as $place): ?>
+                <a class="list-group-item panel-body" href="<?=site_url($href['places']['details'].'/'.$category['id'].'/'.$place['id'])?>">
+                    <div class="row search-panel">
+                        <div class="col-xs-3 col-sm-2 col-md-2">
+                            <img class="img-thumbnail place-panel-img" src="<?=base_url('public/images/places/thumbnails/7-Eleven,_Jalan_Song_Thian_Cheok_thumb.png')?>" alt="something cooler">
+                        </div><!-- col-xs-3 -->
+                        <div class="col-xs-9 col-sm-8 col-md-8 place-panel-detail">
+                            <div class="row">
+                                <div class="col-xs-12 col-md-12 place-panel-name">
+                                    <h4>something cooler</h4>
+                                </div><!-- place-panel-name -->
+                            </div><!-- /.row -->
+                            <div class="row">
+                                <div class="col-xs-12 col-md-12 place-panel-rating">
+                                    <h4>
+                                    <?php for ($i = 0; $i < 5; $i++): ?>
+                                        <?php if ($place['rating'] > 0): ?>
+                                            <span class="glyphicon glyphicon-star"></span>
+                                        <?php else: ?>
+                                            <span class="glyphicon glyphicon-star-empty"></span>
+                                        <?php endif; ?>
+                                        <?php $place['rating']--; ?>
+                                    <?php endfor; ?>
+                                    </h4>
+                                </div><!-- /.col-xs-12 -->
+                            </div><!-- /.row -->
+                        </div><!-- /. place-panel-detail -->
+                        <div class="hidden-xs col-sm-2 col-md-2">
+                            <h1><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></h1>
+                        </div><!-- /.hidden-xs -->
+                    </div><!-- /.search-panel -->
+
+                </a>
+                <?php endforeach ?>
+            </div><!-- /.list-group -->
+        </div><!-- /.panel-primary -->   
+        <!-- ?????????????????????????????????????????????????????????????????????????????????????????????????????/         -->
     </div><!-- /.row -->
 </div><!-- /.container -->
 <?=$navbar?>
