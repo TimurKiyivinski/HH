db3b26c86f3b5d367a0c59dac4b7c7d5532bd3c6
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2015-01-14 20:45:37 +0800

    Evil.

--- application/views/accommodations/detail.php
+++ application/views/accommodations/detail.php
@@ -12,6 +12,38 @@
     </div>
     <!--Picture carousel-->
     <div class="picture-carousel">
+        <div id="picture-carousel" class="carousel slide" data-ride="carousel">
+        <!--Indicators-->
+        <ol class="carousel-indicators">
+            <li data-target="#picture-carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#picture-carousel" data-slide-to="1"></li>
+            <li data-target="#picture-carousel" data-slide-to="2"></li>
+        </ol>
+        
+        <!--Wrapper for slides-->
+        <!--TODO: Load images dynamically-->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img class="picture-carousel-img" src="<?=base_url('public/images/places/accommodation/360_Xpress_City_Centre.png')?>" alt="Xpress City Centre">
+            </div>
+            <div class="item">
+                <img class="picture-carousel-img" src="<?=base_url('public/images/places/accommodation/Abell_Hotel.png')?>" alt="Xpress City Centre">
+            </div>
+            <div class="item">
+                <img class="picture-carousel-img" src="<?=base_url('public/images/places/accommodation/City_Inn.png')?>" alt="Xpress City Centre">
+            </div>
+        </div>
+        
+        <!-- Controls -->
+        <a class="left carousel-control" href="#picture-carousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#picture-carousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
     </div>
     <!--Ratings title-->
     <div class=" details-title bg-primary">
