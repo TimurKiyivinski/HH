3a99b4e35fc8947ce6d1a68807d5a317f67ee951
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2015-01-21 20:21:33 +0800

    Search done!

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -13,7 +13,7 @@ class Search extends CI_Controller {
 
     /**
      * Shows the Search interface
-     */
+     *
     public function index()
     {
         log_msg(__CLASS__, __FUNCTION__, func_get_args());
@@ -29,6 +29,7 @@ class Search extends CI_Controller {
         // output view
         $this->load->view('search', $this->data);
     }
+     */
 
     /**
      * Make a search query
@@ -36,43 +37,50 @@ class Search extends CI_Controller {
      * @param string , search string
      * @return an array of places sorted by category
      */
-    public function query($param = FALSE)
+    public function index()
     {
         log_msg(__CLASS__, __FUNCTION__, func_get_args());
         $this->load->helper('form');
-        if ($param === FALSE)
-        {
-            return null;
-        }
-
-        $this->load->model('category_model');
-        $this->load->model('place_model');
-        $this->load->model('search_model');
-
-        // To name the categories
-        $category_data = $this->category_model->get_all();
-
-        $places_search = $this->search_model->find_all($param);
-
-        // Check if there are any results
-        if (sizeof($places_search) == 0)
+        if($this->input->post('submit'))
         {
             $this->data['results'] = 0;
         }
         else
         {
-            $this->data['results'] = 1;
-            
-            // Sort all the places by category
-            for ($i = 0; $i < sizeof($places_search); $i++)
+            $search = $this->input->post('search');
+
+            $this->load->model('category_model');
+            $this->load->model('place_model');
+            $this->load->model('search_model');
+
+            // To name the categories
+            $category_data = $this->category_model->get_all();
+
+            $places_search = [];
+
+            if ($search != "")
+                $places_search = $this->search_model->find_all($search);
+
+            // Check if there are any results
+            if (sizeof($places_search) == 0)
             {
-                $places[$i] = $this->place_model->get($places_search[$i]);
-                $categories[$places[$i]['category_id']]['name'] = $category_data[$places[$i]['category_id']]['name'];
-                $categories[$places[$i]['category_id']]['places'][] = $places[$i];
+                $this->data['results'] = 0;
             }
-            $this->data['categories'] = $categories;
-        }       
-        
+            else
+            {
+                $this->data['results'] = 1;
+
+                // Sort all the places by category
+                for ($i = 0; $i < sizeof($places_search); $i++)
+                {
+                    $places[$i] = $this->place_model->get($places_search[$i]);
+                    $categories[$places[$i]['category_id']]['name'] = $category_data[$places[$i]['category_id'] - 1]['name'];
+                    $categories[$places[$i]['category_id']]['places'][] = $places[$i];
+                }
+                $this->data['categories'] = $categories;
+            }       
+        }
+
         $this->data['title'] = 'Search';
         $this->data['head'] = $this->load->view('templates/head', $this->data, TRUE);
         $this->data['banner'] = $this->load->view('templates/banner', $this->data, TRUE);
--- application/views/search.php
+++ application/views/search.php
@@ -5,15 +5,13 @@
 </head>
 <body>
 <?=$banner?>
-
-
 <div class="container">
-
     <div class="row search-bar bg-primary">
         <div class="col-xs-12 col-sm-12 col-dm-12">
-            <?=form_open(current_url(), array('method' => 'get', 'class' => 'form-horizontal', 'role' => 'form'))?>
+            <?=form_open(current_url(), array('name' => 'search_form', 'method' => 'post', 'class' => 'form-horizontal', 'role' => 'form'))?>
                 <div class="input-group">
-                    <input name="search" class="text form-control" value="<?=$this->input->get('search')?>" type="search" placeholder="Search...">
+                    <!--<?=form_input(array('name' => 'search', 'class' => 'text form-control', 'value' => '', 'type' => 'search', 'placeholder' => 'Search...')) ?>-->
+                    <input id="search" name="search" class="text form-control" value="<?=$this->input->post('search')?>" type="search" placeholder="Search...">
                     <span class="input-group-btn">
                         <button class="btn btn-primary btn-search">
                             <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
@@ -26,50 +24,48 @@
 <?php if ($results == 1): ?>
         <div class="row">
             <div class="panel panel-primary" id="accordion" role="tablist" aria-multiselectable="true">
-                <div class="panel-heading" role="tab" id="heading<?=url_title($categories['1']['name'], '_')?>">
-                    <a class = "white"data-toggle="collapse" data-parent="#accordion" href="#collapse<?=url_title($categories['1']['name'], '_')?>" aria-expanded="true" aria-controls="collapse<?=url_title($categories['1']['name'], '_')?>">
-                        <h3 class="panel-title"><?=$categories['1']['name']?></h3>
-                    </a>
-                </div><!-- /.panel-heading -->
-
-                <div class="list-group panel-collapse collapse in" id="collapse<?=url_title($categories['1']['name'], '_')?>" role="tabpanel" aria-labelledby="heading<?=url_title($categories['1']['name'], '_')?>">
-                    <?php foreach ($categories['1']['places'] as $place): ?>
-                    <a class="list-group-item panel-body" href="<?=site_url($href['places']['details'].'/'.$place['category_id'].'/'.$place['id'])?>">
-                        <div class="row search-panel">
-                            <div class="col-xs-3 col-sm-2 col-md-2">
-                                <img class="img-thumbnail place-panel-img" src="<?=base_url('public/images/places/thumbnails/Merdeka_Palace_Hotel_thumb.png')?>" alt="<?=$place['name']?>">
-                            </div><!-- col-xs-3 -->
-                            <div class="col-xs-9 col-sm-8 col-md-8 place-panel-detail">
-                                <div class="row">
-                                    <div class="col-xs-12 col-md-12 place-panel-name">
-                                        <h4><?=$place['name']?></h4>
-                                    </div><!-- place-panel-name -->
-                                </div><!-- /.row -->
-                                <div class="row">
-                                    <div class="col-xs-12 col-md-12 place-panel-rating">
-                                        <h4>
-                                        <?php for ($i = 0; $i < 5; $i++): ?>
-                                            <?php if ($categories['1']['places'][0]['rating'] > 0): ?>
-                                                <span class="glyphicon glyphicon-star"></span>
-                                            <?php else: ?>
-                                                <span class="glyphicon glyphicon-star-empty"></span>
-                                            <?php endif; ?>
-                                            <?php $categories['1']['places'][0]['rating']--; ?>
-                                        <?php endfor; ?>
-                                        </h4>
-                                    </div><!-- /.col-xs-12 -->
-                                </div><!-- /.row -->
-                            </div><!-- /. place-panel-detail -->
-                            <div class="hidden-xs col-sm-2 col-md-2">
-                                <h1><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></h1>
-                            </div><!-- /.hidden-xs -->
-                        </div><!-- /.search-panel -->
-
-                    </a>
-
-
-                    <?php endforeach ?>
-                </div><!-- /.list-group -->
+                <?php foreach ($categories as &$category): ?>
+                    <div class="panel-heading" role="tab" id="heading<?=url_title($category['name'], '_')?>">
+                        <a class = "white"data-toggle="collapse" data-parent="#accordion" href="#collapse<?=url_title($category['name'], '_')?>" aria-expanded="true" aria-controls="collapse<?=url_title($category['name'], '_')?>">
+                            <h3 class="panel-title"><?=$category['name']?></h3>
+                        </a>
+                    </div><!-- /.panel-heading -->
+                    <div class="list-group panel-collapse collapse in" id="collapse<?=url_title($category['name'], '_')?>" role="tabpanel" aria-labelledby="heading<?=url_title($category['name'], '_')?>">
+                        <?php foreach ($category['places'] as $place): ?>
+                        <a class="list-group-item panel-body" href="<?=site_url($href['places']['details'].'/'.$place['category_id'].'/'.$place['id'])?>">
+                            <div class="row search-panel">
+                                <div class="col-xs-3 col-sm-2 col-md-2">
+                                    <img class="img-thumbnail place-panel-img" src="<?=base_url('public/images/places/thumbnails/Merdeka_Palace_Hotel_thumb.png')?>" alt="<?=$place['name']?>">
+                                </div><!-- col-xs-3 -->
+                                <div class="col-xs-9 col-sm-8 col-md-8 place-panel-detail">
+                                    <div class="row">
+                                        <div class="col-xs-12 col-md-12 place-panel-name">
+                                            <h4><?=$place['name']?></h4>
+                                        </div><!-- place-panel-name -->
+                                    </div><!-- /.row -->
+                                    <div class="row">
+                                        <div class="col-xs-12 col-md-12 place-panel-rating">
+                                            <h4>
+                                            <?php for ($i = 0; $i < 5; $i++): ?>
+                                                <?php if ($category['places'][0]['rating'] > 0): ?>
+                                                    <span class="glyphicon glyphicon-star"></span>
+                                                <?php else: ?>
+                                                    <span class="glyphicon glyphicon-star-empty"></span>
+                                                <?php endif; ?>
+                                                <?php $category['places'][0]['rating']--; ?>
+                                            <?php endfor; ?>
+                                            </h4>
+                                        </div><!-- /.col-xs-12 -->
+                                    </div><!-- /.row -->
+                                </div><!-- /. place-panel-detail -->
+                                <div class="hidden-xs col-sm-2 col-md-2">
+                                    <h1><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></h1>
+                                </div><!-- /.hidden-xs -->
+                            </div><!-- /.search-panel -->
+                        </a>
+                        <?php endforeach ?>
+                    </div><!-- /.list-group -->
+                <?php endforeach ?>
             </div><!-- /.panel-primary -->    
         </div><!-- /.row -->
 <?php else: ?>
