701e77232afabb32f8ea4091d7c3f5ef0425a293
Sean <4308131@students.swinburne.edu.my>
2015-01-27 23:40:46 +0800

    fixed topbar

--- application/HH/views/templates/topbar.php
+++ application/HH/views/templates/topbar.php
@@ -1,6 +1,6 @@
 <div class="row ">
 <?php foreach ($categories as $category): ?>
-    <div class="col-xs-2 col-md-2 col-sm-2 col-dm-2 center">
+    <div class="col-xs-2 col-md-2 col-sm-2 col-dm-2 center topbar">
         <a class="btn btn-topbar" href="<?=site_url($href['places']['list'].'/'.$area_id.'/'.$category['id'])?>">
             <img class="topbar-btn" src="<?=base_url('public/images/icons/')?><?='/'.url_title($category['name'], '_', TRUE).'_icon.png'?>">
         </a>
