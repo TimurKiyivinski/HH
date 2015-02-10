1c86fce5e1e07c38ef6a440a7a770b2bf27d5664
Timur Lavrenti Kiyivinski <deathseclipse@gmail.com>
2015-02-10 11:56:36 +0800

    Tiny whoops!

--- application/HH/controllers/search.php
+++ application/HH/controllers/search.php
@@ -21,45 +21,38 @@ class Search extends CI_Controller {
     {
         log_msg(__CLASS__, __FUNCTION__, func_get_args());
         $this->load->helper('form');
-        if($this->input->post('submit'))
+        $search = $this->input->post('search');
+
+        $this->load->model('category_model');
+        $this->load->model('place_model');
+        $this->load->model('search_model');
+
+        // To name the categories
+        $category_data = $this->category_model->get_all();
+
+        $places_search = [];
+
+        if ($search != "")
+            $places_search = $this->search_model->find_all($search);
+
+        // Check if there are any results
+        if (sizeof($places_search) == 0)
         {
             $this->data['results'] = 0;
         }
         else
         {
-            $search = $this->input->post('search');
+            $this->data['results'] = 1;
 
-            $this->load->model('category_model');
-            $this->load->model('place_model');
-            $this->load->model('search_model');
-
-            // To name the categories
-            $category_data = $this->category_model->get_all();
-
-            $places_search = [];
-
-            if ($search != "")
-                $places_search = $this->search_model->find_all($search);
-
-            // Check if there are any results
-            if (sizeof($places_search) == 0)
+            // Sort all the places by category
+            for ($i = 0; $i < sizeof($places_search); $i++)
             {
-                $this->data['results'] = 0;
+                $places[$i] = $this->place_model->get($places_search[$i]);
+                $categories[$places[$i]['category_id']]['name'] = $category_data[$places[$i]['category_id'] - 1]['name'];
+                $categories[$places[$i]['category_id']]['places'][] = $places[$i];
             }
-            else
-            {
-                $this->data['results'] = 1;
-
-                // Sort all the places by category
-                for ($i = 0; $i < sizeof($places_search); $i++)
-                {
-                    $places[$i] = $this->place_model->get($places_search[$i]);
-                    $categories[$places[$i]['category_id']]['name'] = $category_data[$places[$i]['category_id'] - 1]['name'];
-                    $categories[$places[$i]['category_id']]['places'][] = $places[$i];
-                }
-                $this->data['categories'] = $categories;
-            }       
-        }
+            $this->data['categories'] = $categories;
+        }       
 
         $this->data['title'] = 'Search';
         $this->data['head'] = $this->load->view('templates/head', $this->data, TRUE);
@@ -81,45 +74,38 @@ class Search extends CI_Controller {
     {
         log_msg(__CLASS__, __FUNCTION__, func_get_args());
         $this->load->helper('form');
-        if($this->input->post('submit'))
+        $search = $this->input->post('search');
+
+        $this->load->model('area_model');
+        $this->load->model('place_model');
+        $this->load->model('search_model');
+
+        // To name the categories
+        $area_data = $this->area_model->get_all();
+
+        $places_search = [];
+
+        if ($search != "")
+            $places_search = $this->search_model->find_all($search);
+
+        // Check if there are any results
+        if (sizeof($places_search) == 0)
         {
             $this->data['results'] = 0;
         }
         else
         {
-            $search = $this->input->post('search');
+            $this->data['results'] = 1;
 
-            $this->load->model('area_model');
-            $this->load->model('place_model');
-            $this->load->model('search_model');
-
-            // To name the categories
-            $area_data = $this->area_model->get_all();
-
-            $places_search = [];
-
-            if ($search != "")
-                $places_search = $this->search_model->find_all($search);
-
-            // Check if there are any results
-            if (sizeof($places_search) == 0)
+            // Sort all the places by category
+            for ($i = 0; $i < sizeof($places_search); $i++)
             {
-                $this->data['results'] = 0;
+                $places[$i] = $this->place_model->get($places_search[$i]);
+                $areas[$places[$i]['area_id']]['name'] = $area_data[$places[$i]['area_id'] - 1]['name'];
+                $areas[$places[$i]['area_id']]['places'][] = $places[$i];
             }
-            else
-            {
-                $this->data['results'] = 1;
-
-                // Sort all the places by category
-                for ($i = 0; $i < sizeof($places_search); $i++)
-                {
-                    $places[$i] = $this->place_model->get($places_search[$i]);
-                    $areas[$places[$i]['area_id']]['name'] = $area_data[$places[$i]['area_id'] - 1]['name'];
-                    $areas[$places[$i]['area_id']]['places'][] = $places[$i];
-                }
-                $this->data['areas'] = $areas;
-            }       
-        }
+            $this->data['areas'] = $areas;
+        }       
 
         $this->data['title'] = 'Search';
         $this->data['head'] = $this->load->view('templates/head', $this->data, TRUE);
